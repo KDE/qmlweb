e0a2f72232f16a763bab62a70bbcba3ff982255f
Anton Kreuzkamp <kde-development@akreuzkamp.de>
2015-07-27 22:01:29 +0200

    Use only one whitespace in copyright statement.
    It's simply prettier.

--- tests/auto/qmljsc/testparserpass.cpp
+++ tests/auto/qmljsc/testparserpass.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015  Jan Marker <jan@jangmarker.de>
+ * Copyright (C) 2015 Jan Marker <jan@jangmarker.de>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
