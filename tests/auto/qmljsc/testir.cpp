08d8c753a14c6d6a3bf2140defc22e2d196ec535
Anton Kreuzkamp <kde-development@akreuzkamp.de>
2015-07-27 21:29:35 +0200

    Consistently use one email address in file headers
    Previously most copyright headers had akreuzkamp@web.de as my e-mail
    address, while some already used kde-development@akreuzkamp.de. Now
    they all use kde-development@akreuzkamp.de.

--- tests/auto/qmljsc/testir.cpp
+++ tests/auto/qmljsc/testir.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015  Anton Kreuzkamp <akreuzkamp@web.de>
+ * Copyright (C) 2015  Anton Kreuzkamp <kde-development@akreuzkamp.de>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
